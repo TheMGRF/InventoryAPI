08c45ed18728ac90b25ab5789458ca6d147f5d1c
TheMGRF <thomas.grf@googlemail.com>
2020-07-16 11:53:48 +0100

    Make Text class package private and protected

--- src/main/java/me/themgrf/inventoryapi/api/Text.java
+++ src/main/java/me/themgrf/inventoryapi/api/Text.java
@@ -2,9 +2,17 @@ package me.themgrf.inventoryapi.api;
 
 import org.bukkit.ChatColor;
 
-public class Text {
+/**
+ * Basic utility class for internal colour translation
+ */
+class Text {
 
-    public static String colour(String msg) {
+    /**
+     * Convert colour codes in a string
+     * @param msg The string to convert
+     * @return The converted string containing colour codes
+     */
+    protected static String colour(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
